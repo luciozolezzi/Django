3b941e3fee18ac23624a93e09cdbf357612c0ce1
lucio.zolezzi <lucio.zolezzi@snoopconsulting.com>
2018-12-19 11:01:06 -0300

    templates para admin e index

--- faceSnoopv2/faceDetection/admin.py
+++ faceSnoopv2/faceDetection/admin.py
@@ -6,9 +6,16 @@ from django.contrib import admin
 from .models import Empleado,FaceRectangle
 
 class EmpleadoAdmin(admin.ModelAdmin):
-    fields=['nombre','legajo','ingreso','egreso']
-
-
+    fieldsets = [
+        ('Nombre del empleado', {'fields': ['nombre']}),
+        ('Numero de legajo', {'fields': ['legajo']}),
+        ('Horario de ingreso', {'fields': ['ingreso']}),
+        ('Horario de egreso', {'fields': ['egreso']}),
+    ]
+    list_display=('nombre','legajo')        #Indica los campos que se muestran y el orden
+    list_filter = ['nombre']                #Añade filtros para el campo nombre
+    search_fields = ['nombre','legajo']     #Añade una caja de busqueda en admin
+    list_per_page=20                        #Empleados mostrados por pagina
 
 
 # Register your models here.
